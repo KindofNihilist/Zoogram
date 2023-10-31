e19333808824ca480ad38040e3c7b876c60e36e9
Artem Dolbiiev <77683768+tmmyxo@users.noreply.github.com>
2023-10-31 16:53:43 +0200

    Removed unneeded fonts.

--- Fonts/CustomFonts.swift
+++ Fonts/CustomFonts.swift
@@ -14,25 +14,16 @@ class CustomFonts {
     }
 
     static func boldFont(ofSize size: CGFloat) -> UIFont? {
-//        return UIFont(name: "Comfortaa-Bold", size: size)
-//        return UIFont(name: "WixMadeforDisplay-Bold", size: size)
-//        return UIFont(name: "RoundedMplus1c-Bold", size: size)
         return UIFont(name: "TTRounds-Bold", size: size)
 //        return UIFont.systemFont(ofSize: size, weight: .bold)
     }
 
     static func regularFont(ofSize size: CGFloat) -> UIFont? {
-//        return UIFont(name: "Comfortaa-Regular", size: size)
-//        return UIFont(name: "WixMadeforDisplay-Regular", size: size)
-//        return UIFont(name: "RoundedMplus1c-Regular", size: size)
         return UIFont(name: "TTRounds-Regular", size: size)
 //        return UIFont.systemFont(ofSize: size, weight: .regular)
     }
 
     static func lightFont(ofSize size: CGFloat) -> UIFont? {
-//        return UIFont(name: "Comfortaa-Light", size: size)
-//        return UIFont(name: "WixMadeforDisplay-Bold", size: size)
-//        return UIFont(name: "RoundedMplus1c-Light", size: size)
         return UIFont(name: "TTRounds-Light", size: size)
 //        return UIFont.systemFont(ofSize: size, weight: .light)
     }
